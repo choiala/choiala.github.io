7544142998a359d2ee8f683193e69406fed10ac9
choiala <chooooiala@gmail.com>
2022-03-22 17:00:06 +0900

    edit meta-config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `choiala.log`,
+  description: `최알라로그`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
-  ogImage: `/og-image.png`, // Path to your in the 'static' folder
+  siteUrl: `https://www.choiala.com`,
+  ogImage: `/chalhotteok.png`, // Path to your in the 'static' folder
   comments: {
-    utterances: {
+    utterances: { //댓글용
       repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `최지영`,
     bio: {
       role: `개발자`,
       description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+      thumbnail: 'chalhotteok2.png', // Path to the image in the 'asset' folder
     },
     social: {
+      // 블로그랑 다른거로 바꾸는 법 찾아보기
       github: ``, // `https://github.com/zoomKoding`,
       linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
       email: ``, // `zoomkoding@gmail.com`,
